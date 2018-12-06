7e34f11ff087f282c4075307b324d6d9aebbb63c
yangchunmao <yangchunmao8805@163.com>
2018-12-06 17:24:38 +0800

    ng-zorro-antd

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -20,6 +20,7 @@ import { DiamondEntityModule } from './entities/entity.module';
 import * as moment from 'moment';
 // jhipster-needle-angular-add-module-import JHipster will add new module here
 import { JhiMainComponent, NavbarComponent, FooterComponent, PageRibbonComponent, ActiveMenuDirective, ErrorComponent } from './layouts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     imports: [
@@ -38,7 +39,8 @@ import { JhiMainComponent, NavbarComponent, FooterComponent, PageRibbonComponent
         DiamondHomeModule,
         DiamondAccountModule,
         // jhipster-needle-angular-add-module JHipster will add new module here
-        DiamondEntityModule
+        DiamondEntityModule,
+        BrowserAnimationsModule
     ],
     declarations: [JhiMainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent],
     providers: [
