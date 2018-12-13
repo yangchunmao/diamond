a5c3368faee51e4aa7f00aa2d55ff1a9652fc247
yangchunmao <yangchunmao8805@163.com>
2018-12-13 18:30:59 +0800

    hasAnyAuthority

--- src/main/webapp/app/layouts/sidebar/sidebar.component.ts
+++ src/main/webapp/app/layouts/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { SettingsService } from '../../shared';
 import { Router } from '@angular/router';
+import { ProfileService } from '../profiles/profile.service';
 
 @Component({
     selector: 'jhi-sidebar',
@@ -10,10 +11,17 @@ import { Router } from '@angular/router';
 export class SidebarComponent implements OnInit {
     @Input()
     isMobile: boolean;
+    inProduction: boolean;
+    swaggerEnabled: boolean;
 
-    constructor(public settings: SettingsService, private router: Router) {}
+    constructor(public settings: SettingsService, private router: Router, private profileService: ProfileService) {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.profileService.getProfileInfo().then(profileInfo => {
+            this.inProduction = profileInfo.inProduction;
+            this.swaggerEnabled = profileInfo.swaggerEnabled;
+        });
+    }
 
     tabClick(commands) {
         this.router.navigate([commands]);
